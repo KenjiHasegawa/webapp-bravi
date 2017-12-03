b181233749130df0dcefa149475ec48a379e2ea4
KenjiHasegawa <kenjihasegawa@live.com>
2017-12-03 14:42:45 -0200

    Database object with connect to database function

---
+++ api/config/database_class.php
@@ -0,0 +1,60 @@
+<?php
+
+class Database{
+
+    // database credentials
+    private $host       = "localhost";
+    private $database   = "contacts";
+    private $username   = "root";
+    private $password   = "";
+
+    // public vars
+    public $connection = null;
+
+    // public funtions
+    // initialize connection to database
+    public function connect(){
+
+        // trying connection to host
+        $this->connection = mysqli_connect($this->host, $this->username, $this->password);
+
+        // expect UTF-8 encoding
+        mysqli_query($this->connection, "SET NAMES utf8");
+
+        // if connection to host fails
+        if($this->connection === false){
+            die("ERROR: Could not connect. " . mysqli_connect_error());
+        }
+
+        // query for creating database if it does not exists yet
+        $sql = "CREATE DATABASE IF NOT EXISTS contacts";
+
+        // if the query fails
+        if(!mysqli_query($this->connection, $sql)){
+            echo "ERROR: Could not execute $sql. " . mysqli_error($this->connection);
+        }
+
+        // trying connection to the database
+        $this->connection = mysqli_connect($this->host, $this->username, $this->password, $this->database);
+
+        // query for creating table of people if it does not exists yet
+        $sql = "CREATE TABLE IF NOT EXISTS people(
+                id INT NOT NULL PRIMARY KEY AUTO_INCREMENT,
+                first_name VARCHAR(30) NOT NULL,
+                last_name VARCHAR(30) NOT NULL,
+                email VARCHAR(70) NOT NULL UNIQUE,
+                phone VARCHAR(70) NOT NULL UNIQUE,
+                whatsapp VARCHAR(70) NOT NULL UNIQUE
+        )";
+
+        // if the query fails
+        if(!mysqli_query($this->connection, $sql)){
+            echo "ERROR: Could not execute $sql. " . mysqli_error($this->connection);
+        }
+
+        return $this->connection;
+    }
+}
+
+//mysqli_close($link);
+?>
